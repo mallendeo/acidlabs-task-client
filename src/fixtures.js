3c776417c54752b53610032f1a816a97a192bdf6
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-12-19 20:30:25 -0300

    add cities backgrounds from unsplash

---
+++ src/fixtures.js
@@ -0,0 +1,26 @@
+export const IMAGES = {
+  Santiago: [
+    'https://images.unsplash.com/photo-1441484295955-db07de1fdbad',
+    'https://images.unsplash.com/photo-1447108636563-00ee74478efb'
+  ],
+  Zurich: [
+    'https://images.unsplash.com/photo-1535964047557-5bad8df9b37a',
+    'https://images.unsplash.com/photo-1515488764276-beab7607c1e6'
+  ],
+  Auckland: [
+    'https://images.unsplash.com/photo-1507699622108-4be3abd695ad',
+    'https://images.unsplash.com/photo-1534473298238-97b36b30d4a7'
+  ],
+  Sydney: [
+    'https://images.unsplash.com/photo-1530276371031-2511efff9d5a',
+    'https://images.unsplash.com/photo-1524293581917-878a6d017c71'
+  ],
+  Londres: [
+    'https://images.unsplash.com/photo-1529180184525-78f99adb8e98',
+    'https://images.unsplash.com/photo-1508711046474-2f4c2d3d30ca'
+  ],
+  Georgia: [
+    'https://images.unsplash.com/photo-1505294399615-2479253a4990',
+    'https://images.unsplash.com/photo-1513266663629-c62b7ccb225a'
+  ]
+}
